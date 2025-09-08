c1e1b945752dc2c97eaa82009f087f7ddf6ae018
Kauan Rodrigues Lima <162514268+Kauanrodrigues01@users.noreply.github.com>
2025-09-07 23:41:19 -0300

    Atualizar o vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true, // permite acessar por IP ou domínio
+    port: 5173, // a porta que você estiver usando (opcional)
+    strictPort: true,
+    allowedHosts: ['teste.kauan.space', 'portfolio.kauan.space'] // adicione seus domínios aqui
+  }
 })
